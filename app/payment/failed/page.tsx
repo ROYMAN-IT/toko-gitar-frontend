d4d95c69c521f709a0bc6ddb2847425a13063624
Pranaya Widi Ramadhan <pranayadiscord@gmail.com>
2026-07-13 17:49:17 +1000

    first commit frontend

--- app/payment/failed/page.tsx
+++ app/payment/failed/page.tsx
@@ -1,8 +1,6 @@
 import { Suspense } from "react";
 import PaymentFailedClient from "./PaymentFailedClient";
 
-export const dynamic = "force-dynamic";
-
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
